8971b21097de869ea24a9c53ca7b868268940234
Jordan <jordan@smartdust.me>
2025-09-26 19:22:59 +0200

    Fixed building web app

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -1,8 +1,8 @@
 cd ../web
-yarn install
+npm install
 export REACT_APP_API_BASE_URL="/dashboard"
-yarn run build
+npm run build
 cd ..
 rm -rf lib/public
 mkdir -p lib/public
-cp -R ./web/build/ ./lib/public/
+cp -R ./web/build/* ./lib/public/
